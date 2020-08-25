700fe4aa8b2ccd82ec46fc6e7b43b300a104afe1
Aayush_Neupane <neupane0403@gmail.com>
2020-08-25 07:24:46 +0545

    subSequence of the string using recursion

---
+++ recursion/subSequenceOfString.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+void solve(std::string input, std::vector<std::string>& output,std::string temp) {
+	if (input.length() == 0) {
+		output.push_back(temp);
+		
+	}
+	else {
+		auto lastElm = input.back();
+		input.pop_back();
+
+		solve(input, output, temp);
+		temp.push_back(lastElm);
+		solve(input, output, temp);
+	}
+
+}
+std::vector<std::string> subSequence(std::string input) {
+	//this step in unimportant
+	std::reverse(input.begin(), input.end());
+	std::vector<std::string>output;
+	std::string temp = "";
+	solve(input, output, temp);
+
+	//sorting is also not important
+	std::sort(output.begin(),output.end());
+	return output;
+}
+
+//test client
+int main() {
+	std::string input = "abc";
+
+	std::vector<std::string> output=subSequence(input);
+
+	for (auto item : output) {
+		if (item.length() == 0) {
+			std::cout << "null string" << std::endl;
+			continue;
+		}
+		std::cout << item << std::endl;
+	}
+}
